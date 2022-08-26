902c479c28cd25341bfd544ead2e0eecc9369102
Olivier Durand <oldu73@gmail.com>
2022-08-26 08:05:53 +0200

    add todo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import "./style.css";
 
 const ul = document.querySelector("ul");
+const form = document.querySelector("form");
+const input = document.querySelector("form > input");
+
+console.log(form, input);
+
+// add todo
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const value = input.value;
+  input.value = "";
+  addTodo(value);
+  displayTodo();
+});
 
 const todos = [
   {
@@ -32,6 +45,13 @@ const createTodoElement = (todo, index) => {
   <button>Delete</button>
   `;
   return li;
+};
+
+const addTodo = (text) => {
+  todos.push({
+    text,
+    done: false,
+  });
 };
 
 displayTodo();
